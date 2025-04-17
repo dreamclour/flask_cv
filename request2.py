888a7e48480dbd761b2067bb24e3817a871da2a4
dreamclour <52489925+dreamclour@users.noreply.github.com>
2025-04-17 16:43:54 +0300

    Update request2.py

--- request2.py
+++ request2.py
@@ -1,19 +1,19 @@
 # -- coding: utf-8 --
 #multipart/form-data 格式传图像，json传文本
 #本地图像
+#传输单张图片，可在本地测试
 import requests
-url = "http://i-1.gpushare.com:21533/detect"
-urllocal = "http://127.0.0.1:5000/detect"
-file_path = "C:/Users/49860/Desktop/yymm.jpg"
+url = "网址"
+urllocal = "本地测试"
+file_path = "图片路径"
 
 response = requests.post(url, files={'file': open(file_path, 'rb')})
-#print(response.json())
 
 if response.status_code == 200:
     # 保存图像到本地
-    with open('C:/Users/49860/Desktop/yymm2.jpg', 'wb') as f:
+    with open(file_path, 'wb') as f:
         f.write(response.content)
     print("Image saved as output.jpg")
 else:
     print("Error:", response.text)
-#print(response.json())  # 打印检测结果
+#print(response.json())

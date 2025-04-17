b5979816340b1b85840f3f8657a231440e0ebe5e
dreamclour <52489925+dreamclour@users.noreply.github.com>
2025-04-17 16:38:38 +0300

    Update request.py

--- request.py
+++ request.py
@@ -1,15 +1,13 @@
 # -- coding: utf-8 --
-#这是最初版本的传输，其中，请求和响应之间的格式有点问题
+#这是最初版本的传输，传输单张图片
 import requests
-url ="http://i-1.gpushare.com:21533/detect"
-image_path ="C:/Users/49860/Desktop/yymm.jpg"
+url ="网址"
+image_path ="路径"
 with open(image_path, "rb") as image_file:
-    # 构造表单数据
-    files = {"image": (image_path, image_file,"image/jpg")}
-    # 发送 POST 请求
+    files = {"image": (image_path, image_file,"image/jpg")}#multipart/form-data类型
     response = requests.get(url, files=files)
-    image_file.close()  # 记得关闭文件
-print(response.status_code)  # 打印状态码
-print(response.json())       # 打印 JSON 响应
+    image_file.close()  
+print(response.status_code)  
+print(response.json())      
 print(response)
 
